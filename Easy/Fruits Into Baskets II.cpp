39c44794cf696ba1342d5e026729f220546a3673
kerolos hani <153551384+keroloshany47@users.noreply.github.com>
2025-08-05 07:25:16 +0300

    Update Fruits Into Baskets II.cpp

--- Easy/Fruits Into Baskets II.cpp
+++ Easy/Fruits Into Baskets II.cpp
@@ -1,8 +1,15 @@
+// LeetCode: 3477. Fruits Into Baskets II
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/fruits-into-baskets-ii/description/?envType=daily-question&envId=2025-08-05
+// Time Complexity: O(n^2)
+// Space Complexity: O(n)
+// Where n = number of fruit types (and baskets)
+
 class Solution {
 public:
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
         int n = fruits.size();
-        vector<bool> used(n, false); // تتبع السلال المستخدمة
+        vector<bool> used(n, false); 
         int unplaced = 0;
 
         for (int i = 0; i < n; ++i) {
@@ -10,8 +17,8 @@ public:
 
             for (int j = 0; j < n; ++j) {
                 if (!used[j] && baskets[j] >= fruits[i]) {
-                    used[j] = true;   // السلة دي بقت مستخدمة
-                    placed = true;   // الفاكهة دي اتحطت
+                    used[j] = true;   
+                    placed = true;  
                     break;
                 }
             }
