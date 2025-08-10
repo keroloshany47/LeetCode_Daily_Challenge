3a5062f5c094d5fa7cd7cbdbef85b1ddee16c298
kerolos hani <153551384+keroloshany47@users.noreply.github.com>
2025-08-10 03:20:16 +0300

    Create 869. Reordered Power of 2.cpp

---
+++ Meduim/869. Reordered Power of 2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    bool isPowerOfTwo(int x) {
+        if (x <= 0) return false;
+        while (x % 2 == 0) {
+            x /= 2;
+        }
+        return x == 1;
+    }
+
+    bool reorderedPowerOf2(int n) {
+        std::string s = std::to_string(n);
+        std::sort(s.begin(), s.end());
+
+        do {
+            if (s[0] == '0') continue; 
+
+            int num = std::stoi(s);
+            if (isPowerOfTwo(num)) return true;
+        } while (std::next_permutation(s.begin(), s.end()));
+
+        return false;
+    }
+};
