f74229ad80edba61305e87c32314afdcc72f2d0d
kerolos hani <153551384+keroloshany47@users.noreply.github.com>
2025-08-14 03:53:06 +0300

    Update 2264. Largest 3-Same-Digit Number in String.cpp

--- Easy/2264. Largest 3-Same-Digit Number in String.cpp
+++ Easy/2264. Largest 3-Same-Digit Number in String.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string largestGoodInteger(string num) {
         string ans = "";
-        for (int i=0; i+2 < num.size();i++ )
+        for (int i=0; i < num.size();i++ )
         if (num[i]==num[i+1]&&num[i+1]==num[i+2])
         {
             string sub = num.substr(i, 3);
