80c6f729af18cf3ce9d065e276d2edd096b1b401
kerolos hani <153551384+keroloshany47@users.noreply.github.com>
2025-08-19 15:57:51 +0300

    Update 2348. Number of Zero-Filled Subarrays.cpp

--- Meduim/2348. Number of Zero-Filled Subarrays.cpp
+++ Meduim/2348. Number of Zero-Filled Subarrays.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long result = 0; 
+        long long count = 0;  
+        
+        for (int num : nums) {
+            if (num == 0) {
+                count++;        
+                result += count; 
+            } else {
+                count = 0;
+            }
+        }
+        return result;
+    }
+};
 
