ef708decaf112a175504b72142116156cb241ca6
kerolos hani <153551384+keroloshany47@users.noreply.github.com>
2025-09-04 10:25:49 +0300

    Create 3516. Find Closest Person.cpp

---
+++ Easy/3516. Find Closest Person.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int min_step = abs(x - z);
+
+        if (min_step == abs(y - z)) {
+            return 0; 
+        } 
+        else if (min_step > abs(y - z)) {
+            return 1; 
+        } 
+        else {
+            return 2; 
+        }
+    }
+};
