49acc06b7a1f88f44dfce8fb484ee089c3ef7680
kerolos hani <153551384+keroloshany47@users.noreply.github.com>
2025-09-05 03:44:52 +0300

    Update 2749. Minimum Operations to Make the Integer Zero.cpp

--- Meduim/2749. Minimum Operations to Make the Integer Zero.cpp
+++ Meduim/2749. Minimum Operations to Make the Integer Zero.cpp
@@ -11,3 +11,30 @@ public:
         return -1;
     }
 };
+
+// my solution 
+int countOnes(long long x) {
+    int cnt = 0;
+    while (x > 0) {
+        if (x % 2 == 1) cnt++; 
+        x = x / 2; 
+    }
+    return cnt;
+}
+
+class Solution {
+public:
+    int makeTheIntegerZero(int num1, int num2) {
+        for (long long k = 1; k <= 60; k++) {
+            long long val = (long long)num1 - (long long)num2 * k;
+
+            if (val < k) continue; 
+
+            int ones = countOnes(val); 
+            if (ones <= k) {
+                return (int)k;
+            }
+        }
+        return -1;
+    }
+};
