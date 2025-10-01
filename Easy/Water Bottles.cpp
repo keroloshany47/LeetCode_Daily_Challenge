c006fcfa0562d3265ebdc87001fc77a46d8fa232
kerolos hani <keroloshani474@gmail.com>
2025-10-01 23:44:17 +0300

    Create Water Bottles.cpp

---
+++ Easy/Water Bottles.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int consumedBottles = 0;
+
+        while (numBottles >= numExchange) {
+            
+            consumedBottles += numExchange;
+            numBottles -= numExchange;
+
+            
+            numBottles++;
+        }
+
+        
+        return consumedBottles + numBottles;
+    }
+};
